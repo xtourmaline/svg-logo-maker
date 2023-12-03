08a0f398e0091b719e66447a830be8ff67fc9a35
Vanessa Trinh <xtourmaline@gmail.com>
2023-12-03 17:04:59 -0500

    finished the shape.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateSVG = require("./utils/generateSVG");
 
-// 
+// questions that will prompt the user
 const questions = [
     {
         name: "text",
@@ -28,3 +28,17 @@ const questions = [
     }
 ]
 
+function writeToFile(fileName, data) {
+    content = generateSVG(data);
+    fs.writeFile(fileName, content, (err) => err ? console.log(err) : console.log('Success!'));
+}
+
+// function to initialize app
+function init() {
+    inquirer.prompt(questions).then(data => {
+        writeToFile("logo.svg", data)
+    });
+}
+
+// function call to initialize app
+init();
